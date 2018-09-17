8de9214bf6410c23c0836d7c3106ab4157ffc2a6
Francisco Ezeqiel Banda <febanda91@gmail.com>
2018-09-17 17:04:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-var animal = 'dog'
+var animal = 'dog';
 
 function myAnimal() {
-  return animal
+  return 'dog';
 }
 
+var animal = 'dog';
+
 function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
-  return animal
+  var animal = 'cat';
+  return animal;
 }
 
 function add2(n) {
   return n + two
 
-  // Feel free to move things around!
+  console.log(Math.random()*1000)
   const two = 2
 }
 
